93a560c09cfdd299ff3bdca065999085aff14e1c
PabloDQuiroga <jtdoe78@gmail.com>
2018-07-26 18:47:41 -0300

    creada interfaz de inicio y derivacion

--- app/src/main/java/com/pablo/persistencia/MainActivity.java
+++ app/src/main/java/com/pablo/persistencia/MainActivity.java
@@ -2,12 +2,44 @@ package com.pablo.persistencia;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button btn_bundle, btn_shared, btn_files, btn_sqlite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_bundle = (Button)findViewById(R.id.btn_bundle);
+        btn_shared = (Button)findViewById(R.id.btn_shared);
+        btn_files = (Button)findViewById(R.id.btn_files);
+        btn_sqlite = (Button)findViewById(R.id.btn_sqlite);
+
+        btn_bundle.setOnClickListener(this);
+        btn_shared.setOnClickListener(this);
+        btn_files.setOnClickListener(this);
+        btn_sqlite.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_bundle:{
+                break;
+            }
+            case R.id.btn_shared:{
+                break;
+            }
+            case R.id.btn_files:{
+                break;
+            }
+            case R.id.btn_sqlite:{
+                break;
+            }
+        }
     }
 }
